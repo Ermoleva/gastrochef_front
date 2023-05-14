841fde4a4c7e2ac2eab00d0fe431bed663481839
Klapeks <sashaKaraguts@gmail.com>
2023-05-14 19:08:40 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import "../styles/globals.scss";
 import React, { useState } from "react";
 import Header from "../components/header/header";
 import Footer from "../components/footer/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Header/>
       <div className="content">
       <Component {...pageProps} />
