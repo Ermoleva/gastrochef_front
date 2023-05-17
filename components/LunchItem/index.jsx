8e175370f92ac13e28da82ce51cc454911c3e63c
Gekonchik <alinakoskodan@gmail.com>
2023-05-17 13:15:23 +0300

    1

--- components/LunchItem/index.jsx
+++ components/LunchItem/index.jsx
@@ -6,14 +6,14 @@ import img from "../../public/images/lunch-img.png"
 
 export default function LunchItem({lunchItem, increase, decrease}){
 
-    const {id, description, title, gram, kcal, priceTotal, count, price,image} = lunchItem
+    const {id, description1, description2,description3, title, gram, kcal, priceTotal, count, price,image} = lunchItem
 
     return(
         <div className={styles.product__item}>
             <Image src={`/images/${image}`} width="577" height="288"
                    alt="product" layout={'raw'} className={styles.product__img}/>
             <div className={styles.product__lunch_wrapp}>
-            <h2 className={styles.product__title}>{title}</h2>
+            <h2 className={styles.product__title}> Бізнес-ланч "{title}"</h2>
             <p className={styles.product__lunch_gramm}>
                 {gram} г
             </p>
@@ -22,7 +22,13 @@ export default function LunchItem({lunchItem, increase, decrease}){
             </p>
             </div>
             <p className={styles.product__item_info}>
-               {description} 
+               1. {description1} 
+            </p>
+            <p className={styles.product__item_info}>
+               2. {description2} 
+            </p>
+            <p className={styles.product__item_info}>
+               3. {description3} 
             </p>
             <div className={styles.product__count}>
                 <CountLunch id={id} price={price} priceTotal={priceTotal} count={count} increase={increase} decrease={decrease}/>
